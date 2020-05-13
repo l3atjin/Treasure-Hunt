037e89d0455cc145dc35447855940a2c8a0b5387
batjin <batjin@umich.edu>
2020-05-13 14:54:33 -0400

    ready for valgrind

--- hunt.cpp
+++ hunt.cpp
@@ -144,7 +144,7 @@ options getMode(int argc, char * argv[]) {
 
 int main(int argc, char* argv[])
 {
-	std::ios_base::sync_with_stdio(false);
+	//std::ios_base::sync_with_stdio(false);
 
 	// store options in a struct and initiliaze classes with given options
 	options mode = getMode(argc, argv);
